db4e02565cc5ca5c6a8fb3fc1c381e7dc0c19999
huynhanh48 <anhvo482004@gmail.com>
2025-12-31 07:32:59 +0700

    fix build-2

--- src/components/Desk.jsx
+++ src/components/Desk.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import { Environment, Lightformer, OrbitControls, PresentationControls, SpotLight } from '@react-three/drei'
 import ModelDesk from "../models/Desk"
@@ -10,12 +10,15 @@ export default function Desk() {
             <Canvas camera={{ position: [20, 5, 15], fov: 30, near: 0.1, far: 1000 }} >
                 <ambientLight intensity={1} />
                 <SpotLight position={[-3, 5, 5]} angle={1} intensity={1} />
-                <PresentationControls polar={[0, 0]} azimuth={[-Math.PI / 20, Math.PI / 20]}
-                >
-                    <group>
-                        <ModelDesk position={[-10, -50, 0]} scale={10} />
-                    </group>
-                </PresentationControls>
+                <Suspense fallback={null}>
+                    <PresentationControls polar={[0, 0]} azimuth={[-Math.PI / 20, Math.PI / 20]}
+                    >
+                        <group>
+                            <ModelDesk position={[-10, -50, 0]} scale={10} />
+                        </group>
+                    </PresentationControls>
+                </Suspense>
+
 
                 <Environment resolution={256}>
                     <Lightformer
